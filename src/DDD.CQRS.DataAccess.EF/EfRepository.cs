6d5852ad296b9fe0ff2c0a1f0c32b1ac532178be
Alexander Loktionov <loktionov129@gmail.com>
2020-10-25 23:43:34 +0300

    data access

---
+++ src/DDD.CQRS.DataAccess.EF/EfRepository.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) DDD.CQRS. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+
+using Microsoft.EntityFrameworkCore;
+using DDD.CQRS.Domain;
+
+namespace DDD.CQRS.DataAccess.EF
+{
+    public class EfRepository<TEntity, TContext> : IRepository<TEntity>
+        where TEntity : class
+        where TContext : DbContext
+    {
+        public EfRepository(TContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+            Set = Context.Set<TEntity>();
+        }
+
+        protected TContext Context { get; }
+
+        public DbSet<TEntity> Set { get; }
+
+        public virtual void Add(TEntity entity)
+        {
+            Set.Add(entity);
+        }
+
+        public virtual void Remove(TEntity entity)
+        {
+            Set.Remove(entity);
+        }
+    }
+}
---
+++ src/DDD.CQRS.DataAccess.EF/EfUnitOfWork.cs
@@ -0,0 +1,65 @@
+﻿// Copyright (c) DDD.CQRS. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+using Microsoft.EntityFrameworkCore;
+using DDD.CQRS.Abstract;
+
+namespace DDD.CQRS.DataAccess.EF
+{
+    public class EfUnitOfWork<TContext> : IUnitOfWork
+        where TContext : DbContext
+    {
+        private bool disposed;
+
+        public EfUnitOfWork(TContext context)
+        {
+            Context = context ?? throw new ArgumentNullException(nameof(context));
+        }
+
+        protected virtual TContext Context { get; private set; }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        public virtual int SaveChanges()
+        {
+            return Context.SaveChanges();
+        }
+
+        public virtual Task<int> SaveChangesAsync()
+        {
+            return Context.SaveChangesAsync();
+        }
+
+        public virtual Task<int> SaveChangesAsync(CancellationToken cancellationToken)
+        {
+            return Context.SaveChangesAsync(cancellationToken);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                if (Context != null)
+                {
+                    Context.Dispose();
+                    Context = null;
+                }
+            }
+
+            disposed = true;
+        }
+    }
+}
