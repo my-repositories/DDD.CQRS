4f4a10696d57d9ce7c4143f7ba56a3e29b03b718
Alexander Loktionov <loktionov129@gmail.com>
2020-10-25 23:44:47 +0300

    infrastructure

---
+++ src/DDD.CQRS.Infrastructure/CommandFactory.cs
@@ -0,0 +1,43 @@
+﻿// Copyright (c) DDD.CQRS. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using DDD.CQRS.Abstract;
+
+namespace DDD.CQRS.Infrastructure
+{
+    public class CommandFactory : ICommandFactory
+    {
+        private readonly Func<Type, object> resolveCallback;
+
+        public CommandFactory(Func<Type, object> resolveCallback)
+        {
+            this.resolveCallback = resolveCallback;
+        }
+
+        public void Execute<TCommand>(TCommand command)
+            where TCommand : class, ICommand
+        {
+            var commandHandlers = Resolve<TCommand>().ToArray();
+            if ((commandHandlers == null) || !commandHandlers.Any())
+            {
+                throw new ArgumentException("Unknown command: \"" + typeof(TCommand).FullName + "\"");
+            }
+
+            foreach (var commandHandler in commandHandlers)
+            {
+                commandHandler.Execute(command);
+                commandHandler.Dispose();
+            }
+        }
+
+        private IEnumerable<ICommandHandler<T>> Resolve<T>()
+            where T : class, ICommand
+        {
+            return this.resolveCallback(typeof(IEnumerable<ICommandHandler<T>>))
+                as IEnumerable<ICommandHandler<T>>;
+        }
+    }
+}
---
+++ src/DDD.CQRS.Infrastructure/QueryFactory.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) DDD.CQRS. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System;
+using DDD.CQRS.Abstract;
+
+namespace DDD.CQRS.Infrastructure
+{
+    public class QueryFactory : IQueryFactory
+    {
+        private readonly Func<Type, object> resolveCallback;
+
+        public QueryFactory(Func<Type, object> resolveCallback)
+        {
+            this.resolveCallback = resolveCallback;
+        }
+
+        public T ResolveQuery<T>()
+            where T : class, IQuery
+        {
+            return resolveCallback(typeof(T)) as T;
+        }
+    }
+}
